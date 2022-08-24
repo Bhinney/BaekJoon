b83d1f0dfb9efbd27ccd215bc443bf476932e4c1
Bhin <107696895+Bhinney@users.noreply.github.com>
2022-08-24 18:04:18 +0900

    [Bronze V] Title: 아스키 코드, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,9 +1,8 @@
-import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.next().charAt(0);
+    public static void main(String[] args) throws IOException{
+        int a = System.in.read();
         
         System.out.println(a);
     }
