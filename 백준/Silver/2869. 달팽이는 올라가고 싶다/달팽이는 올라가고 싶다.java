b903e168d1c241dbba7e0057bba18eff5e9a6798
Bhin <107696895+Bhinney@users.noreply.github.com>
2022-10-04 18:46:10 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -6,16 +6,14 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-        long A = Long.parseLong(st.nextToken()); // 낮에 달팽이가 올라갈 수 있는 길이
-        long B = Long.parseLong(st.nextToken()); // 밤에 달팽이가 미끄러지는 길이
-        long V = Long.parseLong(st.nextToken()); // 달팽이가 올라갈 막대기의 길이
+        int A = Integer.parseInt(st.nextToken()); // 낮에 달팽이가 올라갈 수 있는 길이
+        int B = Integer.parseInt(st.nextToken()); // 밤에 달팽이가 미끄러지는 길이
+        int V = Integer.parseInt(st.nextToken()); // 달팽이가 올라갈 막대기의 길이
         
         br.close();
         
-        long X = (long) Math.floor((V-B)/(A-B)); // 올라간 날
-        
-        if((V-B) % (A-B) != 0){X++;}
-        
+        int X = (int)Math.ceil(((double)(V-B)/(double)(A-B)));// 올라간 날
+                
         System.out.println(X);
     }
 }
