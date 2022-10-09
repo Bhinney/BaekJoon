b6ad7b65e436595fc96ca4a8858cadb84159ca5a
Bhin <107696895+Bhinney@users.noreply.github.com>
2022-10-09 16:38:33 +0900

    [Bronze I] Title: 소인수분해, Time: 208 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(); // 소인수 분해 할 숫자
+        sc.close();
+        
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            while(num % i == 0){
+                System.out.println(i);
+                num /= i;
+            }
+        }
+        if(num != 1){
+            System.out.println(num);
+        }
+    }
+}
