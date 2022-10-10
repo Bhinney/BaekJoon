bb4dbce74ebb865d99091dfd7b28d8f082768b9f
Bhin <107696895+Bhinney@users.noreply.github.com>
2022-10-10 17:37:24 +0900

    [Silver I] Title: 회의실 배정, Time: 512 ms, Memory: 43236 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        int[][] time = new int[n][2];
+        
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            
+            // 시작
+            time[i][0] = Integer.parseInt(st.nextToken());
+            
+            // 종료
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(time, new Comparator<int[]>() {
+			
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				
+				// 종료시간이 같을 경우 시작시간이 빠른순으로 정렬 
+				if(o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				
+				return o1[1] - o2[1];
+			}
+ 
+		});
+        
+        int count = 0;
+		int prev_end_time = 0;
+		
+		for(int i = 0; i < n; i++) {
+			
+			// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신 
+			if(prev_end_time <= time[i][0]) {
+				prev_end_time = time[i][1];
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+    }
+}
